b711da3c03fd43fe8046618fcae90afc614e0239
Linus Weigand <linusweigand@protonmail.com>
2022-03-17 16:50:13 +0100

    Value in Eur & Fee in Euro

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 const APIKEY = '5938SEBM9YJ7JVBMTJF3FIB8IAZHNPZPV4';
 const ADR = '0x240Eb7B9Bde39819E05054EFeB412Ce55250898c';
 const URL_ETHERSCAN = `https://api.etherscan.io/api?module=account&action=txlist&address=${ADR}&startblock=0&endblock=99999999&page=1&offset=100&sort=asc&apikey=${APIKEY}`;
-var i = 0
 
 fetchData();
 
@@ -13,14 +12,10 @@ async function getEthPrice(timeStamp, gData) {
     return await response.json();
 }
 
-calcPrice = (data, gData) => {
+calcPriceThen = (data, gData, i) => {
     const price = parseFloat(data["Data"]["Data"][1]["close"]);
-    console.log('data',gData)
-    const euro = gweiToEth(gData["result"][i++]["value"]) * price;
-    console.log('price',price);
-    console.log('euro',euro);
-
-    return price;
+    const euro = gweiToEth(gData["result"][i]["value"]) * price;
+    return euro;
 }
 
 
@@ -32,7 +27,10 @@ async function fetchData() {
     for(let i=0; i<data.result.length; i++){
         const ethResp = await getEthPrice(data.result[i].timeStamp, data);
         if(ethResp.Data.Data){
-            data.result[i].price = ethResp.Data.Data[0].close;
+            data.result[i].price = ethResp.Data.Data[1].close;
+            data.result[i].valueInEuro = data.result[i].price * gweiToEth(data.result[i].value)
+            data.result[i].fee = data.result[i].gasPrice * data.result[i].gasUsed
+            data.result[i].feeInEuro = data.result[i].price * gweiToEth(data.result[i].fee)
             ethPrices.push(data.result[i]);
         }
     };
@@ -43,15 +41,17 @@ async function fetchData() {
 
 const showGrid = (ethPrices) => {
 
+    console.log(ethPrices)
     const columnDefs = [
         { headerName: 'Date', field: "timeStamp", valueFormatter: (p) => new Date(Number(p.value * 1000)).toISOString().split('T')[0], sortable: true, filter: true },
         { headerName: 'Time', field: "timeStamp", valueFormatter: (p) => new Date(Number(p.value * 1000)).toISOString().split('T')[1].split('.')[0], sortable: true, filter: true, editable: true },
-        { headerName: 'Value', field: "value", valueFormatter: (p) => (gweiToEth(p.value)) + " Ether", sortable: true, filter: true, editable: true },
-        { headerName: 'PreisThen', field: "price", sortable: true, filter: true, editable: true },
+        { headerName: 'Value', field: "value", valueFormatter: (p) => (gweiToEth(p.value)) + " ETH", sortable: true, filter: true, editable: true },
+        { headerName: 'Value In Euro', field: "valueInEuro" ,valueFormatter: (p) => `${Math.round(p.value * 100) / 100} €`, sortable: true, filter: true, editable: true },
      //   { headerName: 'Value in Eur', field: "timeStamp", valueFormatter: (i)=>ethPrices[i], sortable: true, filter: true, editable: true },
         { headerName: 'IN/OUT', field: "to", valueFormatter: (p) => p.value.toLowerCase() === ADR.toLowerCase() ? 'IN' : 'OUT', sortable: true, filter: true },
         { headerName: 'Txn Hash', field: "hash", sortable: true, filter: true, editable: true },
-        { headerName: 'Txn Fee', field: "gasPrice", valueFormatter: (p) => (p.value / Math.pow(10, 18)) + " Ether", sortable: true, filter: true },
+        { headerName: 'Txn Fee', field: "fee", valueFormatter: (p) => (p.value / Math.pow(10, 18)) + " ETH", sortable: true, filter: true, editable: true },
+        { headerName: 'Fee in Euro', field: "feeInEuro", valueFormatter: (p) => `${Math.round(p.value * 100) / 100} €`, sortable: true, filter: true },
     ];
 
     const rowData = ethPrices;
